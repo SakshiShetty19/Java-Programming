9705d4d77156236ad5c22ad025fe355b60f8e592
SakshiShetty19 <sakshishetty126@gmail.com>
2025-05-19 20:48:46 +0530

    Update and rename ArrayList.java to ArrayList_examples/IsRedFound.java

---
+++ ArrayList_examples/IsRedFound.java
@@ -0,0 +1,22 @@
+/*1b. Array List programs
+1. Write a java program for getting different colors through ArrayList interface and search whether
+the color "Red" is available or not*/
+package assignment;
+import java.util.*;
+public class IsRedFound {
+	
+	    public static void main(String[] args) {
+	        ArrayList<String> colors = new ArrayList<>();
+	        colors.add("Red");
+	        colors.add("Green");
+	        colors.add("Blue");
+	        colors.add("Yellow");
+
+	        boolean found = colors.contains("Red");
+	        System.out.println("Is Red present? " + found);
+	    }
+	
+
+}
+
+
