0f18d0980cae6ff084b678a53c658d32858c5440
SakshiShetty19 <sakshishetty126@gmail.com>
2025-05-19 22:04:12 +0530

    Create countOccurrences.java

---
+++ String_Exercise_progams/countOccurrences.java
@@ -0,0 +1,29 @@
+/*Q2. Write a Java Program for Counting how many times a substring appears in a main string
+using user defined function countOccurrences()*/
+
+public class countOccurrences {
+
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Enter the main string: ");
+	        String str = sc.nextLine();
+
+	        System.out.print("Enter the substring to count: ");
+	        String sub = sc.nextLine();
+
+	        int c = 0;
+	        int i = 0;
+
+	        if (str != null && sub != null && !sub.isEmpty()) {
+	            while ((i = str.indexOf(sub, i)) != -1) {
+	                c++;
+	                i += sub.length();
+	            }
+	        }
+
+	        System.out.println("The substring appears " + c + " times.");
+
+	        sc.close();
+	    }
+	}
