6da036fe4e1896b990c30f0d7534d97101c3f520
SakshiShetty19 <sakshishetty126@gmail.com>
2025-05-19 20:52:30 +0530

    Create RemoveColors.java

---
+++ ArrayList_example/RemoveColors.java
@@ -0,0 +1,19 @@
+/*2. Write a java program for getting different colors through ArrayList interface and remove the
+2nd element and color "Blue" from the ArrayList*/
+import java.util.*;
+
+public class RemoveColors
+{
+	public static void main(String[] args) {
+		  ArrayList<String> colors = new ArrayList<>();
+        colors.add("Red");
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+         System.out.println("before removal: " + colors);
+        colors.remove(1); // 2nd element (index 1)
+        colors.remove("Blue");
+
+        System.out.println("After removal: " + colors);
+	}
+}
